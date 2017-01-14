9b664f823a6f209557b3a3a5244636cac0dd1b40
Saad Mazhurul Huque <saadmheleven@gmail.com>
2017-01-14 21:15:16 +0600

    Introduction To Java
    How To Print Java Program

---
+++ My_Hobby.java
@@ -0,0 +1,10 @@
+package My_Info;
+
+public class My_Hobby {
+
+
+    public static void main(String[] args) {
+    
+    System.out.println("My Three Favourites Hobby:\n1.Travel Around Bangladesh.\n2.Photography.\n3.Playing Computer Games.");}
+    
+}
---
+++ My_Info.java
@@ -0,0 +1,9 @@
+package My_Info;
+
+public class My_Info {
+
+    public static void main(String[] args) {
+
+    System.out.println("Name: Saad Mazhurul Huque.\nSchool: Government Muslim High School.\nCollege: BAF Shaheen College.\nUniversity: International Islamic University Chittagong.\nLocation: Eastmadarbari, Chittagong.\nEmail: saadth11san@gmail.com");}
+   
+}
